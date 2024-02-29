f3bede892da020653384c507f59211e12460e3f9
Adam Scherlis <adam@scherlis.com>
2024-02-29 01:27:50 +0000

    shell script improvements

---
+++ cleanup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm vectors/*/*.pt
+rm erasers/*/*.pt
+rm normalized_vectors/*/*.pt
+rm results/*/*.json
+rm results/open_ended_scores/*/*.json
+rm analysis/*.*
+rm analysis/*/*.*
+rm analysis/*/pca/*.png
