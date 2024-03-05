bd10844d3237ef7c35cb94e7d3778d719987752a
Adam Scherlis <adam@scherlis.com>
2024-03-05 03:11:52 +0000

    cleanup logit

--- cleanup.sh
+++ cleanup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 rm vectors/*/*.pt
+rm vectors/*/logit/*.pt
 rm erasers/*/*.pt
+rm erasers/*/logit/*.pt
 rm normalized_vectors/*/*.pt
+rm normalized_vectors/*/logit/*.pt
 rm results/*/*.json
 rm results/open_ended_scores/*/*.json
 rm analysis/*.*
